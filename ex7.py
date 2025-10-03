e35246d77a86d08d490d8986f7340c813e8a664e
Мария Масенко <mariamasenko@Marias-MacBook-Air.local>
2025-10-03 22:52:08 +0300

    lab1 task7: record video from camera view

---
+++ ex7.py
@@ -0,0 +1,27 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+# выбираю декодер для видео
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+output_file = cv2.VideoWriter("output_task7.mp4", fourcc, fps, (width, height))
+
+while True:
+    ret, frame = cap.read()
+    
+    if not(ret):
+        break
+    
+    cv2.imshow('Video', frame)
+    # записываю кадр видео в файл
+    output_file.write(frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()
+output_file.release()
+cv2.destroyAllWindows()
